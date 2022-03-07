48743a46f5dad8003a7c9005689046950ef66991
Sandro Brognara <sandrobrognara@Sandros-MacBook-Pro.local>
2022-03-07 11:38:58 -0500

    making notes about steps, approaches

--- contracts/TransferPets.sol
+++ contracts/TransferPets.sol
@@ -6,6 +6,13 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 contract TransferPets is ERC1155 {
     address owner;
 
+    struct PuppyData {
+        uint256 priceEth;
+        uint256[] transferWithIds;
+    }
+
+    mapping(uint256 => PuppyData) puppyIdsToData;
+
     constructor() public ERC1155("<ipfs uri here>") {
         owner = msg.sender;
         // _mint(msg.sender, puppyId, 1, "");
@@ -15,6 +22,10 @@ contract TransferPets is ERC1155 {
     function buyPuppies(uint256[] calldata _ids, uint256[] calldata _amounts)
         public
     {
+        // * check brother and sister are sold together
+        // * check buyer has enough eth to purchase (msg.amount)
+        // * transfer eth from buyer to contract
+        // * transfer the NFT to msg.sender
         safeBatchTransferFrom(owner, msg.sender, _ids, _amounts, "");
     }
 }
