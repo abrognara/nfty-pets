e7a85f68cd0523b747ebb90c318cdbccc9ebdb8e
Sandro Brognara <sandrobrognara@Sandros-MacBook-Pro.local>
2022-03-09 12:14:40 -0500

    comment out batch function

--- contracts/TransferPets.sol
+++ contracts/TransferPets.sol
@@ -54,20 +54,20 @@ contract TransferPets is ERC1155, Ownable {
 
     // mint batch to owner?
 
-    function buyPuppies(uint256[] calldata _ids, uint256[] calldata _amounts)
-        public
-        payable
-    {
-        // * check buyer has enough eth to purchase (msg.value)
-        uint256 totalPriceEth;
-        for (uint256 i = 0; i < _ids.length; i++) {
-            PuppyData memory puppyData = puppyIdsToData[_ids[i]];
-            totalPriceEth += puppyData.priceEth;
-        }
-        require(msg.value >= totalPriceEth, "Not enough eth sent");
-        // * check brother and sister are sold together
-        // * transfer eth from buyer to contract (or directly to owner?)
-        // * transfer the NFT to msg.sender
-        safeBatchTransferFrom(owner, msg.sender, _ids, _amounts, "");
-    }
+    // function buyPuppies(uint256[] calldata _ids, uint256[] calldata _amounts)
+    //     public
+    //     payable
+    // {
+    //     // * check buyer has enough eth to purchase (msg.value)
+    //     uint256 totalPriceEth;
+    //     for (uint256 i = 0; i < _ids.length; i++) {
+    //         PuppyData memory puppyData = puppyIdsToData[_ids[i]];
+    //         totalPriceEth += puppyData.priceEth;
+    //     }
+    //     require(msg.value >= totalPriceEth, "Not enough eth sent");
+    //     // * check brother and sister are sold together
+    //     // * transfer eth from buyer to contract (or directly to owner?)
+    //     // * transfer the NFT to msg.sender
+    //     safeBatchTransferFrom(owner, msg.sender, _ids, _amounts, "");
+    // }
 }
