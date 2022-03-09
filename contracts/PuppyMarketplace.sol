610fd9cbe0ac278df432b444595cc0fae324aefc
Sandro Brognara <sandrobrognara@Sandros-MacBook-Pro.local>
2022-03-09 16:20:01 -0500

    contract needs to be ERC1155 receiver holder

--- contracts/PuppyMarketplace.sol
+++ contracts/PuppyMarketplace.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 
-contract PuppyMarketplace is Ownable {
+contract PuppyMarketplace is Ownable, ERC1155Holder {
     struct PuppyMarketItem {
         address payable seller;
         address payable owner;
